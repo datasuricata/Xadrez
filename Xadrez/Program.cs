72b1cfba0b297123cf10cb5003d5351d34b6f5fd
lucasrochademoraes <lucas.moraes.dev@gmail.com>
2018-02-26 17:31:50 -0300

    Implementando metodo que imprime o tabuleiro no console

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -8,6 +8,9 @@ namespace Xadrez
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
         }
     }
