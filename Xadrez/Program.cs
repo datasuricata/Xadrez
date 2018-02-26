74d8831cdc06dd6061eedbd9dd4b2154b6fa0721
lucasrochademoraes <lucas.moraes.dev@gmail.com>
2018-02-26 17:08:00 -0300

    Criado classe para posição das peças

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace Xadrez
 {
@@ -10,6 +7,10 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.Write("Posição: " + P);
+            Console.Read();
         }
     }
 }
