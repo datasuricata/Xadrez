0c008de5128b53c8ec78b70e973d9791571d4e1f
lucasrochademoraes <lucas.moraes.dev@gmail.com>
2018-03-14 01:04:33 -0300

    Implementado lógica promoção

--- Xadrez/xadrez/Partida.cs
+++ Xadrez/xadrez/Partida.cs
@@ -145,6 +145,21 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            //jogada_especial_promocao
+            if (p is Peao)
+            {
+                if (p.cor == Cor.Branca && destino.linha == 0 || p.cor == Cor.Preta && destino.linha == 7)
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -164,7 +179,7 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+
             //jogada_especial_enpassant
 
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
